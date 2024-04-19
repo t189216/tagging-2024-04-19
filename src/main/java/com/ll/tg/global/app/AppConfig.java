f025a3f12f6be8e418efb70f0354bbc181786770
T189216 <tus0926@naver.com>
2024-04-20 02:21:17 +0900

    feat: 외부 파일 접근

---
+++ src/main/java/com/ll/tg/global/app/AppConfig.java
@@ -0,0 +1,57 @@
+package com.ll.tg.global.app;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class AppConfig {
+    private static String activeProfile;
+
+    @Value("${spring.profiles.active}")
+    public void setActiveProfile(String activeProfile) {
+        this.activeProfile = activeProfile;
+    }
+
+    public static boolean isProd() {
+        return activeProfile.equals("prod");
+    }
+
+    public static boolean isDev() {
+        return activeProfile.equals("dev");
+    }
+
+    public static boolean isTest() {
+        return activeProfile.equals("Test");
+    }
+
+    public static boolean isNotProd() {
+        return !isProd();
+    }
+
+    @Getter
+    private static String jwtSecretKey;
+
+    @Value("${custom.jwt.secretKey}")
+    public void setJwtSecretKey(String jwtSecretKey) {
+        this.jwtSecretKey = jwtSecretKey;
+    }
+
+    @Getter
+    public static String tempDirPath;
+
+    @Value("${custom.temp.dirPath}")
+    public void setTempDirPath(String tempDirPath) {
+        this.tempDirPath = tempDirPath;
+    }
+
+    @Getter
+    public static String genDirPath;
+
+    @Value("${custom.gen.dirPath}")
+    public void setGenDirPath(String genDirPath) {
+        this.genDirPath = genDirPath;
+    }
+}
---
+++ src/main/java/com/ll/tg/global/webMvc/WebMvcConfig.java
@@ -0,0 +1,15 @@
+package com.ll.tg.global.webMvc;
+
+import com.ll.tg.global.app.AppConfig;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/gen/**")
+                .addResourceLocations("file:///" + AppConfig.getGenDirPath() + "/");
+    }
+}

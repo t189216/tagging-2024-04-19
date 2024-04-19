9bcee2ea7fa94895e24586d4631d4a882981f93f
T189216 <tus0926@naver.com>
2024-04-19 14:29:07 +0900

    chore: start setting

---
+++ src/main/java/com/ll/tg/Tagging20240419Application.java
@@ -0,0 +1,13 @@
+package com.ll.tg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tagging20240419Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tagging20240419Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/tg/Tagging20240419ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.tg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tagging20240419ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

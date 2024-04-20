5c2427f6ae7f2db81f3929dfff33756a05c8cef8
T189216 <tus0926@naver.com>
2024-04-20 20:21:29 +0900

    fix

--- src/main/java/com/ll/tg/domain/controller/HomeController.java
+++ src/main/java/com/ll/tg/domain/controller/HomeController.java
@@ -1,23 +1,13 @@
 package com.ll.tg.domain.controller;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
-    @Value("${custom.jwt.secretKey}")
-    private String jwtSecretKey;
 
     @GetMapping("/")
     public String home() {
         return "domain/home";
-    }
-
-    @GetMapping("/jwtSecretKey")
-    @ResponseBody
-    public String showJwtSecretKey() {
-        return jwtSecretKey;
     }
 }
